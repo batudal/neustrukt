c2ad15141150d5d547170b64886198f04b5a383f
Batu Dal <batudal@gmail.com>
2021-06-02 15:00:17 +0300

    Signed-off-by: Batu Dal <batudal@gmail.com>

--- js/main.js
+++ js/main.js
@@ -25,53 +25,8 @@ function mytext() {
   return tl;
 };
 mytext();
-// gsap.to(".s1", {y: -40, duration: 0.5,delay:1});
-// gsap.to(".s2", {x: 40, duration: 0.5,delay:1});
-// gsap.to(".s3", {y: 40, duration: 0.5,delay:1});
-// gsap.to(".s4", {x: -40, duration: 0.5,delay:1});
 
-// gsap.to(".wholecube", {rotate:90, duration: 0.5, delay:1.5});
 
-// gsap.to(".s1", {y: 0, duration: 0.5,delay:2});
-// gsap.to(".s2", {x: 0, duration: 0.5,delay:2});
-// gsap.to(".s3", {y: 0, duration: 0.5,delay:2});
-// gsap.to(".s4", {x: 0, duration: 0.5,delay:2});
-
-// gsap.to(".wholecube", {rotate:3600/4, duration: 2, delay:2.5, ease:"power1.inOut"});
-(function() {
-    const blurProperty = gsap.utils.checkPrefix("filter"),
-          blurExp = /blur\((.+)?px\)/,
-          getBlurMatch = target => (gsap.getProperty(target, blurProperty) || "").match(blurExp) || [];
-  
-    gsap.registerPlugin({
-      name: "blur",
-      get(target) {
-        return +(getBlurMatch(target)[1]) || 0;
-      },
-      init(target, endValue) {
-        let data = this,
-            filter = gsap.getProperty(target, blurProperty),
-            endBlur = "blur(" + endValue + "px)",
-            match = getBlurMatch(target)[0],
-            index;
-        if (filter === "none") {
-          filter = "";
-        }
-        if (match) {
-          index = filter.indexOf(match);
-          endValue = filter.substr(0, index) + endBlur + filter.substr(index + match.length);
-        } else {
-          endValue = filter + endBlur;
-          filter += filter ? " blur(0px)" : "blur(0px)";
-        }
-        data.target = target; 
-        data.interp = gsap.utils.interpolate(filter, endValue); 
-      },
-      render(progress, data) {
-        data.target.style[blurProperty] = data.interp(progress);
-      }
-    });
-  })();
 
 function s1() {
     let tl = gsap.timeline({
@@ -130,6 +85,14 @@ function s1() {
     return tl;
   }
 
+
+s1()
+s2()
+s3()
+s4()
+cube()
+cube2()
+
 //   function colors() {
 //     let tl = gsap.timeline({
 //         repeat: -1,
@@ -161,23 +124,39 @@ function s1() {
 //     return tl;
 //   }
 
-s1()
-s2()
-s3()
-s4()
-cube()
-cube2()
-// colors()
-// midcube()
 
-
-// function s2() {
-//     let tl = gsap.timeline();
-//     tl.to(".s2", {x: 40, duration: 0.5}).to(".s2", {x: 0, duration: 0.5, delay: 1});
-//     return tl;
-//   }
+// gsap.to(".wholecube", {rotate:3600/4, duration: 2, delay:2.5, ease:"power1.inOut"});
+// (function() {
+//     const blurProperty = gsap.utils.checkPrefix("filter"),
+//           blurExp = /blur\((.+)?px\)/,
+//           getBlurMatch = target => (gsap.getProperty(target, blurProperty) || "").match(blurExp) || [];
   
-  
-//   let master = gsap.timeline()
-//     .add(s1())
-//     .add(s2())
+//     gsap.registerPlugin({
+//       name: "blur",
+//       get(target) {
+//         return +(getBlurMatch(target)[1]) || 0;
+//       },
+//       init(target, endValue) {
+//         let data = this,
+//             filter = gsap.getProperty(target, blurProperty),
+//             endBlur = "blur(" + endValue + "px)",
+//             match = getBlurMatch(target)[0],
+//             index;
+//         if (filter === "none") {
+//           filter = "";
+//         }
+//         if (match) {
+//           index = filter.indexOf(match);
+//           endValue = filter.substr(0, index) + endBlur + filter.substr(index + match.length);
+//         } else {
+//           endValue = filter + endBlur;
+//           filter += filter ? " blur(0px)" : "blur(0px)";
+//         }
+//         data.target = target; 
+//         data.interp = gsap.utils.interpolate(filter, endValue); 
+//       },
+//       render(progress, data) {
+//         data.target.style[blurProperty] = data.interp(progress);
+//       }
+//     });
+//   })();
