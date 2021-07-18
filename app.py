d0895888e26201acb6e10ca6d93a9723c3162d3e
T490s <batudal@gmail.com>
2021-07-18 17:24:56 +0300

    tablename

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"].replace('post
 db = SQLAlchemy(app)
 
 class Subscribers(db.Model):
+    __tablename__ = 'subscribers'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), nullable=False)
 
